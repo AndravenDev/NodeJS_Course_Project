47f2dd4a10d3d48ca5e3981418b9863e926d5b6b
aivan1 <78039586+aivan1@users.noreply.github.com>
2022-11-28 19:35:33 +0200

    completed CRUD with the ORM

--- controllers/products.js
+++ controllers/products.js
@@ -56,7 +56,7 @@ exports.getProductsPage = (req, res, next) => {
 
 exports.getDetailsPage = (req, res, next) => {
   const id = req.params.id;
-  Product.findAll({where: { id: id }})
+  Product.findAll({ where: { id: id } })
     .then((products) => {
       res.render("shop/product-details", {
         product: products[0],
@@ -74,28 +74,43 @@ exports.getAddProductPage = (req, res, next) => {
 };
 
 exports.getEditProductPage = (req, res, next) => {
-  Product.getProductById(req.params.id, (product) => {
-    res.render("admin/edit-product", {
-      pageTitle: "Edit some stuff",
-      product: product,
-      editing: true,
-    });
-  });
+  const id = req.params.id;
+  Product.findAll({ where: { id: id } })
+    .then((products) => {
+      res.render("admin/edit-product", {
+        product: products[0],
+        pageTitle: "Edit some stuff",
+        editing: true,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.saveUpdatedProduct = (req, res, next) => {
   const { id, title, price, imageUrl, desc } = req.body;
-  const updatedProduct = new Product(id, title, imageUrl, price, desc);
-  console.log(updatedProduct);
-  updatedProduct.save();
-  res.redirect("/");
+
+  Product.findAll({ where: { id: id } })
+    .then((data) => {
+      const product = data[0];
+      product.title = title;
+      product.price = price;
+      product.imageUrl = imageUrl;
+      product.description = desc;
+      return product.save();
+    })
+    .then((data) => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getAdminProductPage = (req, res, next) => {
   Product.findAll()
     .then((data) => {
-      console.log("DATA ", data);
-      res.render("admin/admin-products", { prods: data, pageTitle: "Products" });
+      res.render("admin/admin-products", {
+        prods: data,
+        pageTitle: "Products",
+      });
     })
     .catch((err) => console.log(err));
 };
@@ -103,12 +118,17 @@ exports.getAdminProductPage = (req, res, next) => {
 exports.saveProducts = (req, res, next) => {
   const { title, imageUrl, price, desc } = req.body;
   Product.create({ title, imageUrl, price, description: desc })
-    .then((res) => {})
+    .then((data) => {
+      res.redirect("/admin-products");
+    })
     .catch((err) => console.log(err));
 };
 
 exports.deleteProduct = (req, res, next) => {
   const productId = req.params.id;
-  Product.deleteById(productId);
-  res.redirect("/admin-products");
+  Product.destroy({ where: { id: productId } })
+    .then((data) => {
+      res.redirect("/admin-products");
+    })
+    .catch((err) => console.log(err));
 };
